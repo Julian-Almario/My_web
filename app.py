161857d12dc4e88ac00641b4dc69f0b533c9616b
Julian Almario <doctorz.0525@gmail.com>
2025-10-19 15:06:42 -0500

    init commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/portafolio')
+def portafolio():
+    return render_template('portafolio.html')
+
+@app.route('/trabajos')
+def trabajos():
+    return render_template('work.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
